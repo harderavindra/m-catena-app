37af31f9e66bd4bfeeb769b79b0e9340c790402a
ravi <ravindra@stavyah.com>
2025-03-28 00:41:46 +0530

    test context

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -10,33 +10,12 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // Check authentication status on app load
-  useEffect(() => {
-    const checkAuth = async () => {
-      console.log(API_BASE_URL, 'API_BASE_URL')
-      try {
-        const response = await axios.get(`${API_BASE_URL}/api/auth/me`, { withCredentials: true });
-        console.log("Auth Check", response.data.user)
-        setUser(response.data.user);
-      }  catch (error) {
-        if (error.response && error.response.status === 401) {
-          console.warn("User not logged in.");
-        } else {
-          console.error("Auth check error:", error);
-        }
-        setUser(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    checkAuth();
-  }, []);
+ 
 
   // Login function
   const login = async (email, password) => {
-    await axios.post(`${API_BASE_URL}/api/auth/login`, { email, password }, { withCredentials: true });
-    const response = await axios.get(`${API_BASE_URL}/api/auth/me`, { withCredentials: true });
-    setUser(response.data.user);
+    await axios.post(`${API_BASE_URL}/api/auth/login`, { email, password });
+  
   };
 
   // Logout function
