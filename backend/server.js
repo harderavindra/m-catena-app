246420367c0372e057dc75d1845215bc98720680
ravi <ravindra@stavyah.com>
2025-03-27 19:43:56 +0530

    test server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Backend is running...");
+    res.send("Backend is running2...");
 });
 
 const PORT = process.env.PORT || 4000;
